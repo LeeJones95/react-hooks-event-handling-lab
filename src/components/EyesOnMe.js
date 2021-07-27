cef3bb9ab026815188fe18425b511ddb5c10027d
LeeJones95 <leonna.jones18@gmail.com>
2021-07-26 22:49:45 -0400

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    
+    
+    return (
+    <input
+        onFocus={(e) => {
+        console.log('Good!');
+        }}
+        placeholder="onFocus is triggered when you click this input."
+        onBlur={(e) => {
+            console.log('Hey! Eyes on me!')
+    />
+    
+
+        }
+    
+    
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+function Keypad () {
+    function handleChange(event) {
+        console.log('Entering password...')
+    }
+    return (
+
+
+        <div>
+            <input type="password"
+            onChange={handleChange}
+            />
+        </div>
+    )
+}
+
+export default Keypad;
